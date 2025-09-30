db1d452702e188f79a56cf2b13d7bd31ed6ce39b
Gustavo Marin <gustavosmsg18@gmail.com>
2025-09-30 19:54:23 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Configuration //anotation para configuracao
+@Profile("test") //Especifica para perfil de teste
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired // o Spring vai resolver a dependencia e associar uma instancia de userRepository aqui dentro
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		
+		userRepository.saveAll(Arrays.asList(u1,u2)); //salva essa linsta no banco
+	}
+	
+}
