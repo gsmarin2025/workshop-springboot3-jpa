01dddd798f87eee694ec185718b246754840b7d5
Gustavo Marin <gustavosmsg18@gmail.com>
2025-10-09 19:29:34 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -0,0 +1,67 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable { // Serializable serve para marcar (é uma marker interface) que uma
+												// classe pode ter seus objetos convertidos em uma sequência de bytes,
+												// pra objetos trafegar em rede, possa ser gravados em arquivos
+
+private static final long serialVersionUID = 1L; // numero de serie padrao
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	private String name;
+
+	public Category() {
+
+	}
+
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
