269c967c119f35cebb34d2aab3084f7e812e537f
Gustavo Marin <gustavosmsg18@gmail.com>
2025-10-12 19:05:42 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -36,6 +39,10 @@ public class Product implements Serializable{ // Serializable serve para marcar 
 	
 	private Set<Category> categories = new HashSet<>(); //Uso set pro mesmo produto nao pode ter uma mesma categoria mais uma vez
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	
 	public Product() {
 		super();
 	}
@@ -96,6 +103,15 @@ public class Product implements Serializable{ // Serializable serve para marcar 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	@JsonIgnore
+	public Set<Order>getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
